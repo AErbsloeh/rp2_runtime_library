b501645d4a5d6f5943160a00c97d6bd00198fa75
Louise Odumah <louiseodumah12@gmail.com>
2026-05-26 08:22:20 +0200

    Add a standalone CRC16 module

---
+++ hal/crc/crc.h
@@ -0,0 +1,14 @@
+#ifndef HAL_CRC_CRC_H_
+#define HAL_CRC_CRC_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+/*! \brief Compute CRC-16-CCITT for a byte buffer.
+ *  \param data  Byte buffer to checksum
+ *  \param len   Number of bytes in the buffer
+ *  \return      16-bit CRC value
+ */
+uint16_t crc16_ccitt(const uint8_t *data, size_t len);
+
+#endif // HAL_CRC_CRC_H_
