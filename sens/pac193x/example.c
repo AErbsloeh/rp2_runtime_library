846719bcaf23a612aef15fcf184bb42a57891aca
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-08-12 15:39:10 +0200

    fix(pac193x4): Update example

--- sens/pac193x/example.c
+++ sens/pac193x/example.c
@@ -7,41 +7,43 @@
 int main(){   
     stdio_init_all();
     sleep_ms(1000);
+
+    set_gpio_default_led(2);
     init_default_led();
 
     // --- Init of the I2C---
     static i2c_rp2_t i2c0_inst = {
         .i2c_mod = i2c1,
-        .pin_sda = 6,
-        .pin_scl = 7,
+        .pin_sda = 10,
+        .pin_scl = 11,
         .fi2c_khz = 100,
         .avai_devices = 0,
         .init_done = false,
     };
     init_i2c_module(&i2c0_inst);
+    toggle_state_default_led();
     scan_i2c_bus_for_device(&i2c0_inst);
 
     // --- Init of the PAC193x ---
-    static pac193x_t pac193x_config = {
+    static pac193x_t sens = {
         .i2c = &i2c0_inst,
         .gpio_pwrdwn = 255,
         .gpio_alert = 255,
-        .adr = 0,
-        .num_channels = 0,
+        .adr = 0x11,
+        .num_channels = 4,
         .sample_rate = 3,
         .enable_channels = true,
         .enable_sleep_mode = false,
         .enable_single_shot_mode = false,
         .init_done = false
     };
-    pac193x_config.adr = pac193x_get_i2c_address(499);
 
     set_state_default_led(true);
-    bool init_done = pac193x_init(&pac193x_config);
+    bool init_done = pac193x_init(&sens);
     if(init_done){
         printf("Init PAC193x sensor done\n");
     } else {
-        printf("Init failed")
+        printf("Init failed");
     }
 
     uint32_t accumulation_number = 0;
@@ -52,23 +54,23 @@ int main(){
     int16_t current_avg[4] = {0};
     uint64_t power_acc[4] = {0};
 
-    while (true) {  
+    while (true) {
         sleep_ms(2000);
         if(init_done){
-            pac193x_do_conversion(&pac193x_config);
-            accumulation_number = pac193x_read_accumulation_number(&pac193x_config);
-            for (uint8_t i = 0; i < pac193x_config.num_channels; i++){
-                voltage[i] = pac193x_read_voltage_single(&pac193x_config, false, i);
-                current[i] = pac193x_read_current_single(&pac193x_config, false, i);
-                power[i] = pac193x_read_power_single(&pac193x_config, i);
-                voltage_avg[i] = pac193x_read_voltage_single(&pac193x_config, true, i);
-                current_avg[i] = pac193x_read_current_single(&pac193x_config, true, i);
-                power_acc[i] = pac193x_read_power_accumulated(&pac193x_config, i);
+            pac193x_do_conversion(&sens);
+            accumulation_number = pac193x_read_accumulation_number(&sens);
+            for (uint8_t i = 0; i < sens.num_channels; i++){
+                voltage[i] = pac193x_read_voltage_single(&sens, false, i);
+                current[i] = pac193x_read_current_single(&sens, false, i);
+                power[i] = pac193x_read_power_single(&sens, i);
+                voltage_avg[i] = pac193x_read_voltage_single(&sens, true, i);
+                current_avg[i] = pac193x_read_current_single(&sens, true, i);
+                power_acc[i] = pac193x_read_power_accumulated_single(&sens, i);
             }
 
             printf("===========Data =======\n");
             printf("Ite #: %d\n", accumulation_number);
-            for (uint8_t i = 0; i < pac193x_config.num_channels; i++){
+            for (uint8_t i = 0; i < sens.num_channels; i++){
                 printf("V-CH%d: %d\n", i, voltage[i]);
                 printf("I-CH%d: %d\n", i, current[i]);
                 printf("P-CH%d: %d\n", i, power[i]);
@@ -80,4 +82,3 @@ int main(){
         };
     };
 }
-
